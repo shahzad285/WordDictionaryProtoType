d9c368c6ff9b4b111de7fffce295bb73347bb5c0
DESKTOP-S59BJJM\personal <shahzadahamad285@gmail.com>
2024-09-08 21:37:19 +0530

    Implemented the first function

--- WordDictionaryProtoType/Controllers/DictionaryController.cs
+++ WordDictionaryProtoType/Controllers/DictionaryController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WordDictionaryProtoType.Helper;
 
 namespace WordDictionaryProtoType.Controllers
 {
@@ -7,5 +8,27 @@ namespace WordDictionaryProtoType.Controllers
     [Route("[controller]")]
     public class DictionaryController : ControllerBase
     {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public DictionaryController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpGet(Name = "GetDefinition")]
+        public async Task<IActionResult> Get(string word)
+        {
+            string filePath = Path.Combine(_webHostEnvironment.WebRootPath, "Dictionary.txt");
+            // Check if the file exists
+            if (!System.IO.File.Exists(filePath))
+            {
+                return NotFound("File not found.");
+            }
+
+            string meaning = DictionaryFile.ReadDictionary(filePath, word.ToLower().Trim());
+
+            // Return the content as a response
+            return Ok(meaning);
+           
+        }
     }
 }
